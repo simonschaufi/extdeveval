9126f074dcd3939ccd4f7db0713f4b7f9c3dafb7
Oliver Hader <oliver.hader@typo3.org>
2011-02-07 14:33:07 +0000

    Cleanup: Raised version number to 3.2-dev
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/extdeveval/trunk@43216 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "extdeveval"
+# Extension Manager/Repository config file for ext "extdeveval".
 #
-# Auto generated 22-03-2006 01:51
+# Auto generated 07-02-2011 15:31
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -27,17 +27,17 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'author' => 'Kasper Skårhøj',
-	'author_email' => 'kasper@typo3.com',
-	'author_company' => 'Curby Soft Multimedia',
+	'author' => 'TYPO3 v4 Core Team',
+	'author_email' => '',
+	'author_company' => '',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'version' => '3.0.1',
-	'_md5_values_when_last_written' => 'a:50:{s:8:".project";s:4:"7162";s:9:"ChangeLog";s:4:"1056";s:43:"class.tx_extdeveval_fetchContentTopMenu.php";s:4:"fde7";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"d537";s:15:"ext_php_api.dat";s:4:"cf6e";s:14:"ext_tables.php";s:4:"34c2";s:22:"apidocs/t3lib_api.html";s:4:"8116";s:25:"apidocs/t3lib_befunc.html";s:4:"33e1";s:21:"apidocs/t3lib_db.html";s:4:"9fb9";s:22:"apidocs/t3lib_div.html";s:4:"0b01";s:25:"apidocs/t3lib_extmgm.html";s:4:"149c";s:30:"apidocs/tslib_content_api.html";s:4:"c3d5";s:29:"apidocs/tslib_pibase_api.html";s:4:"d37a";s:23:"apidocs/typo3_lang.html";s:4:"eb26";s:27:"apidocs/typo3_template.html";s:4:"d160";s:19:"apidocs/CVS/Entries";s:4:"a7f6";s:22:"apidocs/CVS/Repository";s:4:"bf8e";s:16:"apidocs/CVS/Root";s:4:"a97b";s:11:"CVS/Entries";s:4:"22b5";s:14:"CVS/Repository";s:4:"ed80";s:8:"CVS/Root";s:4:"a97b";s:12:"doc/TODO.txt";s:4:"0a23";s:14:"doc/manual.sxw";s:4:"845c";s:15:"doc/CVS/Entries";s:4:"b429";s:18:"doc/CVS/Repository";s:4:"34ca";s:12:"doc/CVS/Root";s:4:"a97b";s:13:"mod1/PEAR.php";s:4:"d61d";s:39:"mod1/class.tx_extdeveval_apidisplay.php";s:4:"9772";s:39:"mod1/class.tx_extdeveval_cachefiles.php";s:4:"4b9b";s:33:"mod1/class.tx_extdeveval_calc.php";s:4:"b32a";s:40:"mod1/class.tx_extdeveval_cssanalyzer.php";s:4:"cf46";s:38:"mod1/class.tx_extdeveval_highlight.php";s:4:"1eb2";s:39:"mod1/class.tx_extdeveval_iconlister.php";s:4:"a0ee";s:35:"mod1/class.tx_extdeveval_ll2xml.php";s:4:"db9f";s:39:"mod1/class.tx_extdeveval_llxmlsplit.php";s:4:"3914";s:35:"mod1/class.tx_extdeveval_phpdoc.php";s:4:"27ab";s:40:"mod1/class.tx_extdeveval_submodgetll.php";s:4:"0308";s:33:"mod1/class.tx_extdeveval_tmpl.php";s:4:"a1c5";s:41:"mod1/class.tx_extdeveval_tunebeautify.php";s:4:"5c8c";s:37:"mod1/class.tx_extdeveval_tunecode.php";s:4:"d4a2";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"5f02";s:19:"mod1/ext_emconf.php";s:4:"6ebe";s:14:"mod1/index.php";s:4:"7c4b";s:22:"mod1/locallang_mod.xml";s:4:"a6f2";s:19:"mod1/moduleicon.gif";s:4:"8074";s:16:"mod1/CVS/Entries";s:4:"b962";s:19:"mod1/CVS/Repository";s:4:"205d";s:13:"mod1/CVS/Root";s:4:"a97b";}',
+	'version' => '3.2-dev',
+	'_md5_values_when_last_written' => 'a:48:{s:9:"ChangeLog";s:4:"435e";s:8:"bomb.png";s:4:"1a79";s:46:"class.tx_extdeveval_additionalBackendItems.php";s:4:"04d3";s:33:"class.tx_extdeveval_exception.php";s:4:"694b";s:43:"class.tx_extdeveval_fetchContentTopMenu.php";s:4:"71d1";s:12:"devlinks.css";s:4:"de82";s:11:"devlinks.js";s:4:"a1b4";s:16:"ext_autoload.php";s:4:"c192";s:12:"ext_icon.gif";s:4:"7e21";s:17:"ext_localconf.php";s:4:"70c5";s:15:"ext_php_api.dat";s:4:"cf6e";s:14:"ext_tables.php";s:4:"2eec";s:11:"patch.patch";s:4:"32ce";s:23:"registerToolbarItem.php";s:4:"4747";s:22:"apidocs/t3lib_api.html";s:4:"1c61";s:25:"apidocs/t3lib_befunc.html";s:4:"33e1";s:21:"apidocs/t3lib_db.html";s:4:"9fb9";s:22:"apidocs/t3lib_div.html";s:4:"0b01";s:25:"apidocs/t3lib_extmgm.html";s:4:"149c";s:30:"apidocs/tslib_content_api.html";s:4:"c3d5";s:29:"apidocs/tslib_pibase_api.html";s:4:"d37a";s:23:"apidocs/typo3_lang.html";s:4:"eb26";s:27:"apidocs/typo3_template.html";s:4:"c0a2";s:12:"doc/TODO.txt";s:4:"0a23";s:14:"doc/manual.sxw";s:4:"845c";s:13:"mod1/PEAR.php";s:4:"e554";s:39:"mod1/class.tx_extdeveval_apidisplay.php";s:4:"0595";s:50:"mod1/class.tx_extdeveval_buildautoloadregistry.php";s:4:"8212";s:39:"mod1/class.tx_extdeveval_cachefiles.php";s:4:"e3f9";s:33:"mod1/class.tx_extdeveval_calc.php";s:4:"466c";s:40:"mod1/class.tx_extdeveval_cssanalyzer.php";s:4:"fec4";s:38:"mod1/class.tx_extdeveval_highlight.php";s:4:"c457";s:39:"mod1/class.tx_extdeveval_iconlister.php";s:4:"6763";s:35:"mod1/class.tx_extdeveval_ll2xml.php";s:4:"0d23";s:39:"mod1/class.tx_extdeveval_llxmlsplit.php";s:4:"b888";s:35:"mod1/class.tx_extdeveval_phpdoc.php";s:4:"86ff";s:36:"mod1/class.tx_extdeveval_rawedit.php";s:4:"e0e0";s:36:"mod1/class.tx_extdeveval_sprites.php";s:4:"1080";s:40:"mod1/class.tx_extdeveval_submodgetll.php";s:4:"12fb";s:33:"mod1/class.tx_extdeveval_tmpl.php";s:4:"966b";s:41:"mod1/class.tx_extdeveval_tunebeautify.php";s:4:"f8ac";s:37:"mod1/class.tx_extdeveval_tunecode.php";s:4:"d4a2";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"5f02";s:19:"mod1/ext_emconf.php";s:4:"6ebe";s:14:"mod1/index.php";s:4:"b234";s:22:"mod1/locallang_mod.xml";s:4:"a6f2";s:19:"mod1/moduleicon.gif";s:4:"8074";}',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '3.0.0-',
-			'typo3' => '3.7.0-',
+			'php' => '5.2.0-0.0.0',
+			'typo3' => '4.2.0-0.0.0',
 		),
 		'conflicts' => array(
 		),
